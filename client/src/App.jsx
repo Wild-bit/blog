d158a2ebb9078e222cae2d16cd7efdc3e805f0c1
Wild-bit <lai1397119446qq.com>
2023-09-25 23:35:14 +0800

    feat:添加关键页面路由，使用createBrowserRouter创建路由
    react-router-dom v6.16

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,60 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+// 组件采用大驼峰命名
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Single",
+        element: <Single />,
+      },
+      {
+        path: "/Write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="app">
+      <div className="containr">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
